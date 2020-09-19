615fa331557c0ba29ca54425b104a22a38317e06
Carly Robison <robison@squareup.com>
2020-09-19 13:15:19 -0700

    Reordering things

--- js/fire.js
+++ js/fire.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-  console.log('loading!');
-    let scene = document.getElementById('scene');
-  console.log('scene', scene);
-
-  let flamezz = document.getElementsByClassName('flame-outline')
-  for (let i = 0; i < flamezz.length; i++) {
-    f = flamezz[i]
-    console.log((i/flamezz.length).toString());
-  }
-
+  let scene = document.getElementById('scene');
   var parallax = new Parallax(scene, {
     relativeInput: true
   });
