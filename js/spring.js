65abadc99502a58cb86e00648465c67157d9e9ed
mumu <58455483+anti-rocker@users.noreply.github.com>
2020-09-19 15:05:28 -0700

    added id spring to canvas
    changed canvas

---
+++ js/spring.js
@@ -0,0 +1,65 @@
+
+function setup() {
+  let cnv = createCanvas(1350, 400);
+  background(87, 210, 214,);
+cnv.id('spring');
+ noStroke();
+
+ let c = color (235, 235, 84)
+  fill(c);
+ ellipse(240, 140, 350, 350);
+  noStroke();
+
+c = color(111, 214, 92)
+  fill (c);
+  rect(572, 200, 10, 300)
+
+  c = color(242, 242, 245)
+  fill(c);
+  translate(580, 200);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  c = color(235, 235, 84);
+  fill(c);
+  ellipse(0, 0, 50, 50);
+
+  c = color(111, 214, 92)
+  fill (c);
+  rect(295, 50, 10, 300)
+
+
+  c = color(242,242,245)
+  fill (c);
+  translate(300, 50);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+
+  c = color(235, 235, 84);
+  fill(c);
+  ellipse(0, 0, 50, 50);
+
+c = color(111, 214, 92)
+  fill (c);
+  rect(295, 50, 10, 300)
+
+
+  c = color(242,242,245)
+  fill (c);
+  translate(300, 50);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+
+  c = color(235, 235, 84);
+  fill(c);
+  ellipse(0, 0, 50, 50);
+
+}
