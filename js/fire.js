ed7915e207a24940ba58e926ffd07e2c1363e33c
Carly Robison <robison@squareup.com>
2020-09-19 13:52:33 -0700

    Making Carly's stuff work

--- js/fire.js
+++ js/fire.js
@@ -1,9 +1,6 @@
-// <script src='js/libs/jquery.min.js'></script>
 
 
-// $(document).ready(function() {
-//   let scene = document.getElementById('scene');
-//   var parallax = new Parallax(scene, {
-//     relativeInput: true
-//   });
-// });
+$(document).ready(function() {
+  let scene = document.getElementById('scene');
+  var parallax = new Parallax(scene);
+});
